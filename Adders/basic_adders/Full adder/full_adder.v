f9a62f3e7b6cf7b4a142e61110c54d67ad0412b3
prabalraj18 <prblraj18@gmail.com>
2025-02-02 23:27:26 +0530

    Removed all folder, Adding again

--- Adders/basic_adders/Full adder/full_adder.v
+++
@@ -1,7 +0,0 @@
-module full_adder (input a ,b ,Cin ,
-	            output sum ,carry );
-	            
-	assign sum = a ^ b ^ Cin ;
-	assign carry = (a & b) | (b & Cin) | (Cin & a);
-	
-endmodule	 	            
